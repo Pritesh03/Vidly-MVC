2a861e5dbbaafa44bc6a815c054de71017f67a7e
Pritesh <p.rajendra.patil@duckcreek.com>
2018-03-16 13:19:15 +0530

    Vidly So Far

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() {Id = 10001, Name = "The Jungle Book" };
+            List<Customer> lstCustomer = new List<Customer>() {
+                new Customer(){ Id = 50001, Name = "Sheldon Cooper"},
+                new Customer(){ Id = 50002, Name = "Kaylee Cucoo"}
+            };
+
+            MovieCustomersViewModel movieCustomers = new MovieCustomersViewModel() {
+                Movie = movie,
+                Customers = lstCustomer
+            };
+
+
+            return View(movieCustomers);
+        }        
+        
+    }
+}
