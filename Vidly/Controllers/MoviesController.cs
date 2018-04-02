dca9ae2a63fe94acc2ffb32886bd74954d67540f
Pritesh <p.rajendra.patil@duckcreek.com>
2018-04-02 17:57:21 +0530

    Building Forms End

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -46,12 +46,47 @@ namespace Vidly.Controllers
             var genres = _context.Genres.ToList();
 
             var viewModel = new NewMovieViewModel() {
-
+                Genres = genres
             };
 
-            return View();
+            return View("MovieForm", viewModel);
         }
 
+        //Save the movie to Database
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+               
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
 
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.DateAdded = movie.DateAdded;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.GenreId = movie.GenreId;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //Edit a Movie
+        public ActionResult Edit(int? Id)
+        {
+            var movie = _context.Movies.Single(m => m.Id == Id);
+
+            var viewModel = new NewMovieViewModel() {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+           return View("MovieForm", viewModel);
+        }
     }
 }
